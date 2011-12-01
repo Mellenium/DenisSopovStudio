c9062f82c3d6dd92f84cc2507fd4cce4aaf8afc6
Corvin89 <corvin8989@gmail.com>
2011-12-01 15:45:41 +0200

    Removed link back from page-portfolio.php #5

--- app/wp-content/themes/markup/page-portfolio.php
+++ app/wp-content/themes/markup/page-portfolio.php
@@ -2,7 +2,6 @@
 <div id="content-internal">
     <div id="content-internal-header">
     	<div id="content-internal-header-title">Portfolio</div>
-		<div id="back-link" align="center"><a href='javascript:history.go(-1)'>← Back</a></div>
         <div style="clear:both"></div>
 	</div>
     <div id="our-works-center">
