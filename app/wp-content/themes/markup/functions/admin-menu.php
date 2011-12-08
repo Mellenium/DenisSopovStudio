851a6d5cdab8ce0c995fc57d5c770a8bb908d616
Spiritvoin <Spiritvoin88@gmail.com>
2011-12-08 17:15:54 +0200

    The settings of contacts in the form of options, subject #27

--- app/wp-content/themes/markup/functions/admin-menu.php
+++ app/wp-content/themes/markup/functions/admin-menu.php
@@ -2,45 +2,70 @@
 // Создать пользовательское меню
 add_action('admin_menu', 'omr_create_menu');
 
-function omr_create_menu() {
+function omr_create_menu()
+{
 
 
-//создать новое меню верхнего уровня
-add_menu_page('Build Internet Settings', 'Build Internet', 'administrator', 
-__FILE__, 'omr_settings_page', 'favicon.ico');
+    //создать новое меню верхнего уровня
+    add_menu_page('Build Internet Settings', 'Build Internet', 'administrator',
+                  __FILE__, 'omr_settings_page', 'favicon.ico');
 
-//вызвать функцию register settings
-add_action( 'admin_init', 'register_mysettings' );
+    //вызвать функцию register settings
+    add_action('admin_init', 'register_mysettings');
 
-function register_mysettings() {
-//регистрируем наши настройки
-register_setting( 'omr-settings-group', 'omr_tracking_code' );
+    function register_mysettings()
+    {
+        //регистрируем наши настройки
+        register_setting('omr-settings-group', 'omr_tracking_code');
+         register_setting('omr-settings-group', 'ICQ');
+         register_setting('omr-settings-group', 'Skype');
+         register_setting('omr-settings-group', 'Email');
+    }
+
+    function omr_settings_page()
+    {
+        ?>
+    <div class="wrap">
+        <h2>Build Internet Options</h2>
+
+        <form method="post" action="options.php">
+
+            <?php settings_fields('omr-settings-group'); ?>
+            <table class="form-table">
+
+                <tr valign="top">
+                    <th scope="row">Tracking Code</th>
+                    <td><textarea name="omr_tracking_code"><?php echo get_option('omr_tracking_code');?></textarea>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">ICQ</th>
+                    <td><textarea name="ICQ"><?php echo get_option('ICQ'); ?></textarea>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Skype</th>
+                    <td><textarea name="Skype"><?php echo get_option('Skype'); ?></textarea>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Email</th>
+                    <td><textarea name="Email"><?php echo get_option('Email'); ?></textarea>
+                    </td>
+                </tr>
+            </table>
+
+            <p class="submit">
+                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>"
+                        />
+            </p>
+
+
+        </form>
+    </div>
+    <?php
+    }
 }
-
-function omr_settings_page() {
-?>
-<div class="wrap">
-<h2>Build Internet Options</h2>
-
-<form method="post" action="options.php">
-
-<?php settings_fields('omr-settings-group'); ?>
-<table class="form-table">
-
-<tr valign="top">
-<th scope="row">Tracking Code</th>
-<td><textarea name="omr_tracking_code"><?php echo get_option('omr_tracking_code'); 
-?></textarea></td>
-</tr>
-
-</table>
-
-<p class="submit">
-<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" 
-/>
-</p>
-
-
-</form>
-</div>
-<?php }}
--- app/wp-content/themes/markup/page-contact.php
+++ app/wp-content/themes/markup/page-contact.php
@@ -6,9 +6,9 @@
     </div>
     <div id="content-internal-center">
         <div class="contacts">
-            <div class="icq"><span class="yellow">ICQ</span><?php echo get_user_meta('1', 'icq', true);?></div>
-            <div class="skype"><span class="yellow">Skype</span><?php echo get_user_meta('1', 'skype', true);?></div>
-            <div class="email"><span class="yellow">Email</span><?php echo $current_user->user_email; ?></div>
+            <div class="icq"><span class="yellow">ICQ</span><?php echo get_option('ICQ');?></div>
+            <div class="skype"><span class="yellow">Skype</span><?php echo get_option('Skype');?></div>
+            <div class="email"><span class="yellow">Email</span><?php echo  get_option('Email'); ?></div>
             <div style="clear:both;"></div>
         </div>
         <?php echo do_shortcode('[contact-form-7 id="75" title="Contact"]') ?>
