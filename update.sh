ea559fdb829fde14a790c8aa9e85199af4cc6149
unknown < @.(none)>
2011-12-10 19:52:26 +0200

    Updated update script

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!sh
+git stash
 git pull origin master
-mysql -uroot -e "drop database denis"
-mysql -uroot -e "create database denis;"
-mysql -uroot denis < backup/database.sql
-mysql -uroot -e "UPDATE wp_options SET option_value = 'http://denis.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" denis
+mysql -uroot -ppassword -e "drop database denis;"
+mysql -uroot -ppassword -e "create database denis;"
+mysql -uroot -ppassword denis < backup/database.sql
+mysql -uroot -ppassword -e "UPDATE wp_options SET option_value = 'http://denis.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" denis
+git stash pop
+
